d52396faadd3ad9cb9f55c8874bba729f91f3b1c
Bryan <bryanmartz972@gmail.com>
2021-10-14 18:36:56 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.set('port',3001);
+//app.set('port',process.env.port || 3001 );
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.set('json space' , 2);
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor iniciado en el puerto 3001');
+});
