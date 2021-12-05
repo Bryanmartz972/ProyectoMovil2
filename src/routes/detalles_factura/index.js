a17330b64cfb7f8a94ecf81781d19a86be5ec3a1
Otoniel Aguirre <otoniel18a@gmail.com>
2021-12-05 10:59:09 -0600

    controladores modificados

--- src/routes/detalles_factura/index.js
+++ src/routes/detalles_factura/index.js
@@ -7,5 +7,5 @@ router.get('/listar2',controladorDetalles_Factura.listardetalle);
 router.get('/listar',controladorDetalles_Factura.listarDetalle_Factura);
 router.post('/guardar',controladorDetalles_Factura.GuardarDetalles_Factura);
 router.delete('/:iddetalles_Factura', controladorDetalles_Factura.EliminarDetalles_Factura);
-router.put('/modificar',controladorAutenticacion.validarAutenticado, controladorDetalles_Factura.ModificarDetalles_Factura);
+router.put('/modificar', controladorDetalles_Factura.ModificarDetalles_Factura);
 module.exports = router;
